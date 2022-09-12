9b708f8f23f92bb2fb41b1a37faa635d34a7890f
otterp012 <otterp012@gmail.com>
2022-09-12 13:23:09 +0900

    add READ ME

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { withContentlayer } = require("next-contentlayer");
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["velog.velcdn.com"],
+    domains: ["velog.velcdn.com", "images.velog.io"],
   },
 };
 module.exports = withContentlayer(nextConfig);
