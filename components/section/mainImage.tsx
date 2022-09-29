01745f9749f025e569c2a784703fc3e8837f53d7
otterp012 <otterp012@gmail.com>
2022-09-29 20:56:26 +0900

    fix thumbnail Image style

--- components/section/mainImage.tsx
+++ components/section/mainImage.tsx
@@ -7,11 +7,11 @@ type Props = {
 
 const MainImage: React.FC<Props> = ({ thumbnailImg, title }) => {
   return (
-    <div className='mx-auto mt-5 h-[200px] w-[70%] md:h-[420px]'>
+    <div className='mx-auto mt-5 min-h-[200px] w-[70%] md:h-[420px]'>
       <Image
         src={thumbnailImg}
         alt={title}
-        className='h-full w-full object-cover'
+        className='h-full w-full object-scale-down'
         width={1000}
         height={1000}
       />
