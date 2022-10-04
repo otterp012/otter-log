9da03cfc1184ad4dc5def18985f8f326e791d216
otterp012 <otterp012@gmail.com>
2022-10-04 15:40:30 +0900

    fix: dark모드범위가 한정지어지는 부분

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,9 +11,9 @@ export default class MyDocument extends Document {
       `;
 
     return (
-      <Html className='over-flow-x-hidden max-w-full' lang='ko'>
+      <Html className='over-flow-x-hidden max-w-full dark:bg-black' lang='ko'>
         <Head />
-        <body className='over-flow-x-hidden max-w-full dark:bg-black dark:text-white'>
+        <body className='over-flow-x-hidden h-full max-w-full dark:bg-black dark:text-white'>
           <script dangerouslySetInnerHTML={{ __html: setThemeMode }} />
           <Main />
           <NextScript />
