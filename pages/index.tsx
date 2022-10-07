1068078fc6371cf9f1b28774207874728405428e
otterp012 <otterp012@gmail.com>
2022-10-08 01:32:56 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,32 @@ const Home: NextPage<Props> = ({ featuredPost, recentPosts }) => {
         slug={featuredPost.path}
         cardType='featuredCard'
       />
-      {/* <RecentPosts recentPosts={recentPosts} /> */}
+      <div className='mx-auto mt-10 md:max-w-[70%]'>
+        <h2 className='mb-8 text-center text-3xl font-bold italic md:mb-10'>
+          RECENT POSTS
+        </h2>
+        {recentPosts.map(
+          ({
+            title,
+            description,
+            publishedAtFormatted,
+            thumbnailImg,
+            path,
+            tags,
+          }: PostType) => (
+            <Card
+              title={title}
+              description={description}
+              publishedAt={publishedAtFormatted}
+              thumbnailImg={thumbnailImg}
+              key={title}
+              slug={path}
+              tags={tags}
+              cardType='verticalCard'
+            />
+          ),
+        )}
+      </div>
     </Fragment>
   );
 };
