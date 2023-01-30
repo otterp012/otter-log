57661defd9735fbfcb46fa3c6ded79bd11b48409
otterp012 <otterp012@gmail.com>
2023-01-30 17:16:01 +0900

    fix: header 컴포넌트 수정, 테스트코드 수정

--- components/Header/DarkModeButton.test.tsx
+++ components/Header/DarkModeButton.test.tsx
@@ -1,24 +1,33 @@
-import { render, screen } from "@testing-library/react";
+import { render, RenderResult } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import DarkModeButton from "./DarkModeButton";
 
 describe("dark mode", () => {
-  it("darkMode 버튼이 렌더링 된다.", () => {
-    render(<DarkModeButton />);
+  let rendered: RenderResult;
+  let button: HTMLElement;
 
-    const Button = screen.getByRole("button", { name: /DARK/i });
-    expect(Button);
+  beforeEach(() => {
+    // arrange
+    rendered = render(<DarkModeButton />);
+    button = rendered.getByRole("button");
+  });
+
+  it("darkMode 버튼이 문제없이 렌더링 되어야 한다.", () => {
+    expect(button).toBeInTheDocument();
   });
 
   it("darkMode 버튼을 클릭하면 다크모드로 바뀌고, 다시한번 누르면 라이트 모드로 바뀐다.", async () => {
-    render(<DarkModeButton />);
-    const Button = screen.getByRole("button", { name: /DARK/i });
-    await userEvent.click(Button);
+    // act
+    await userEvent.click(button);
 
-    expect(Button).toHaveAccessibleName("LIGHT");
-    await userEvent.click(Button);
+    // assert
+    expect(button).toHaveAccessibleName("LIGHT");
 
-    expect(Button).toHaveAccessibleName("DARK");
+    // act
+    await userEvent.click(button);
+
+    // assert
+    expect(button).toHaveAccessibleName("DARK");
   });
 });
--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -3,11 +3,9 @@ import { CustomLink } from "components/CustomLink";
 import { memo } from "react";
 const Logo = () => {
   return (
-    <CustomLink href='/'>
-      <h1 aria-label='otter-log' className='text-3xl font-extrabold italic'>
-        OTTER-LOG
-      </h1>
-    </CustomLink>
+    <h1 aria-label='otter-log' className='text-3xl font-extrabold italic'>
+      <CustomLink href='/'>OTTER-LOG</CustomLink>
+    </h1>
   );
 };
 
