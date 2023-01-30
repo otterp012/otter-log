fa60947789265cce1d7c2bc68e300a14f2eeb59a
otterp012 <otterp012@gmail.com>
2023-01-30 17:19:56 +0900

    fix: card 컴포넌트 디자인 변경, 컴포넌트 전반 재설계

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -4,20 +4,14 @@ import Image from "next/future/image";
 import { CustomLink } from "components/CustomLink";
 import { Tags } from "components/Tags";
 
-// style
-import { CardStyles } from "./styles";
-
 export type Props = {
   title: string;
   description: string;
   publishedAt?: string;
-  thumbnailImg: string;
+  thumbnailImg: string | undefined;
   slug: string;
-  isFeatured?: boolean;
-  tags?: string[];
-  cardType: "verticalCard" | "featuredCard";
+  tags: string[];
 };
-
 const Card: React.FC<Props> = ({
   title,
   publishedAt,
@@ -25,44 +19,35 @@ const Card: React.FC<Props> = ({
   thumbnailImg,
   slug,
   tags,
-  cardType,
 }) => {
-  const style = CardStyles[cardType];
-  const isVerticalCard = cardType === "verticalCard";
-
   return (
-    <section className={style.section}>
-      <CustomLink href={slug}>
-        <Image
-          src={thumbnailImg}
-          alt={title}
-          width={1000}
-          height={1000}
-          className={style.image}
-          placeholder={isVerticalCard ? "blur" : undefined}
-          blurDataURL={
-            isVerticalCard
-              ? "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMMDPT/DwAD3gHy5v4ozQAAAABJRU5ErkJggg=="
-              : undefined
-          }
-          loading={isVerticalCard ? "lazy" : "eager"}
-        />
-      </CustomLink>
-      <div className={style.textWrapper}>
-        <div className={style.textUpperWrapper}>
-          <time className='text-gray-400 text-xs font-bold'>{publishedAt}</time>
-          <CustomLink href={slug}>
-            {isVerticalCard ? (
-              <h3 className={style.title}>{title}</h3>
-            ) : (
-              <h2 className={style.title}>{title}</h2>
-            )}
-          </CustomLink>
-          {isVerticalCard && tags && <Tags tags={tags} />}
+    <li className='mx-auto mb-5 h-96 w-[95%] list-none rounded-lg bg-light-card shadow-lg dark:bg-dark-card sm:w-[48%] lg:w-[32%]'>
+      <Image
+        src={thumbnailImg ?? ""}
+        alt={title}
+        width={1000}
+        height={1000}
+        className='h-[50%] rounded-t-lg object-cover'
+        blurDataURL={
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMMDPT/DwAD3gHy5v4ozQAAAABJRU5ErkJggg=="
+        }
+      />
+      <div className='mt-4 flex flex-col items-center'>
+        <time className='font-semi-bold text-xs'>{publishedAt}</time>
+        <h2 className='mb-2 text-center text-xl font-bold'>{title}</h2>
+        <p className='text-sm'>{description}</p>
+        <div className='mt-2'>
+          <Tags tags={tags} />
         </div>
-        <p className={style.description}>{description}</p>
+        <span
+          className='mt-5 rounded-md bg-light-bg px-5 py-2 font-semibold shadow-2xl hover:bg-deepGray dark:bg-dark-bg hover:dark:bg-deepGray'
+          role='link'
+          aria-label={title}
+        >
+          <CustomLink href={`post/${slug}`}>READ MORE</CustomLink>
+        </span>
       </div>
-    </section>
+    </li>
   );
 };
 
