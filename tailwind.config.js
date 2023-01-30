080401ba58e696ed3399963b6ab8174995139667
otterp012 <otterp012@gmail.com>
2023-01-30 17:21:19 +0900

    chore: 새로운 디펜던시 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
   darkMode: "class",
   theme: {
     colors: {
+      light: {
+        bg: "#d1d5db",
+        text: "#374151",
+        card: "#f3f4f6",
+      },
+      dark: {
+        bg: "#1f2937",
+        text: "#e2e8f0",
+        card: "#334155",
+      },
       black: "#000",
       white: "#fff",
       pink: "#fca5a5",
@@ -13,7 +23,9 @@ module.exports = {
       blue: "#3b82f6",
       deepBlue: "#1d4ed8",
       lightGray: "#d1d5db",
+      gray: "#f3f4f6",
       deepGray: "#64748b",
+      base: "#1e293b",
     },
   },
   plugins: [
