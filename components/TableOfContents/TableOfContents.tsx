c8f3d53b215c2a11267ad97c89b3494bbe2da83d
otterp012 <otterp012@gmail.com>
2023-01-30 17:19:10 +0900

    fix: toc 컴포넌트 수정

--- components/TableOfContents/TableOfContents.tsx
+++ components/TableOfContents/TableOfContents.tsx
@@ -1,56 +1,48 @@
 // hook
 import { useToc } from "hooks";
-import { linkHandler, scrollTopHandler } from "lib/lib";
-
+import { ToTopButton } from "components/ToTopButton";
 type HeadingType = {
   slug: string;
-  heading: "heading2" | "heading3" | "heading4";
+  heading: "heading2" | "heading3";
   text: string;
 };
 
 type props = {
   headings: HeadingType[];
-  title: string;
 };
 
-const TableOfContents: React.FC<props> = ({ headings, title }) => {
+const TableOfContents = ({ headings }: props) => {
   const { visibleList } = useToc();
-
   return (
-    <aside className='relative hidden xl:block'>
-      <div className='border-gray-200 sticky top-[200px] right-5 ml-10 w-[280px] border-l-2'>
-        <div className='py-2 pl-5'>
-          <h4 className='keep-all hover:text-yellow-300 hover-change-color mb-3 inline-block cursor-pointer text-xl font-bold'>
-            <a onClick={scrollTopHandler}>{title}</a>
-          </h4>
-          <ol className='ml-2'>
-            {headings.map(({ slug, heading, text }) => {
-              const visibleStyle =
-                slug === visibleList
-                  ? "font-bold text-deepBlue dark:text-deepPink"
-                  : "";
-              const headingStyle = {
-                heading2: "",
-                heading3: "ml-3 text-sm",
-                heading4: "ml-5 text-xs",
-              };
-              const commonStyle = `keep-all mb-1 h-full w-[250px] pointer hover-change-color`;
+    <div className='mx-auto space-y-2 pb-7 lg:sticky lg:top-[200px] lg:mt-10 lg:w-[25%] lg:self-start lg:pl-3'>
+      <span className='text-lg font-semibold'>📖 목차</span>
+      <ol className='space-y-1.5'>
+        {headings.map(({ slug, heading, text }) => {
+          const visibleStyle = slug === visibleList ? "bg-deepGray" : "";
+          const headingStyle = {
+            heading2: "text-md pl-5",
+            heading3: "pl-8 text-sm",
+          };
 
-              return (
-                <li key={slug}>
-                  <a
-                    onClick={(e) => linkHandler(e, slug)}
-                    className={`${commonStyle} ${headingStyle[heading]} ${visibleStyle}`}
-                  >
-                    {text}
-                  </a>
-                </li>
-              );
-            })}
-          </ol>
-        </div>
-      </div>
-    </aside>
+          return (
+            <li key={slug}>
+              <a
+                onClick={(e) => {
+                  document &&
+                    document.getElementById(`${slug}`)?.scrollIntoView({
+                      behavior: "smooth",
+                    });
+                }}
+                className={`${headingStyle[heading]} pointer block rounded-md ${visibleStyle} hover:bg-deepGray`}
+              >
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ol>
+      <ToTopButton />
+    </div>
   );
 };
 
