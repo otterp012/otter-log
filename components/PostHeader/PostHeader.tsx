621abefce0932951b06a38b2f35baeeca0b598f2
otterp012 <otterp012@gmail.com>
2023-01-30 17:18:55 +0900

    feat: Post header 컴포넌트 추가

---
+++ components/PostHeader/PostHeader.tsx
@@ -0,0 +1,39 @@
+import { Tags } from "components/Tags";
+import { MetaData } from "types/types";
+import Image from "next/future/image";
+
+const PostHeader: React.FC<MetaData> = (props) => {
+  const { title, cover, tags, last_edit, date } = props;
+  return (
+    <header className='space-y-3 border-b border-b-lightGray pb-8 dark:border-b-deepGray'>
+      <h2
+        className='keep-all mb-5 text-center text-4xl font-bold md:text-5xl'
+        id='top'
+      >
+        {title}
+      </h2>
+      {cover && (
+        <Image
+          src={cover}
+          alt={title}
+          className='mx-auto h-auto w-[90%] rounded-xl object-cover md:w-[640px]'
+          width={1000}
+          height={1000}
+        />
+      )}
+      <div className='space-y-2 py-3 pb-5 text-center text-deepGray'>
+        <div>
+          <Tags tags={tags} />
+        </div>
+        <time className='mr-2 inline text-xs'>{date}</time>
+        <span className='text-xs'>by otter</span>
+        <em className='block text-xs'>
+          {last_edit}에 최종수정되었습니다. <br />
+          잘못된 내용이 있으면 댓글을 달아주세요.
+        </em>
+      </div>
+    </header>
+  );
+};
+
+export default PostHeader;
