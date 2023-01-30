76c21d4c281ed9e8d31205bd27a613cbd6241eab
otterp012 <otterp012@gmail.com>
2023-01-30 17:22:05 +0900

    feat: 재사용 가능한 type 추가

--- types/types.ts
+++ types/types.ts
@@ -3,3 +3,26 @@ import { ParsedUrlQuery } from "querystring";
 export type Params = {
   slug: string | string[] | ParsedUrlQuery | undefined;
 };
+
+export type MetaData = {
+  title: string;
+  description: string;
+  date: string;
+  cover: string | undefined;
+  slug: string;
+  last_edit: string;
+  tags: string[];
+  featured: boolean | undefined;
+};
+
+export type HeadingType = {
+  slug: string;
+  heading: "heading2" | "heading3";
+  text: string;
+};
+
+export type PostType = {
+  metadata: MetaData;
+  markdown: string;
+  headings: HeadingType[];
+};
