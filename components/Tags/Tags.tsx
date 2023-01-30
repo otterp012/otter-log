3c0f99c8b355cd277b891d7c136190e67c4607ce
otterp012 <otterp012@gmail.com>
2023-01-30 17:17:29 +0900

    fix: tags 컴포넌트 디자인 수정

--- components/Tags/Tags.tsx
+++ components/Tags/Tags.tsx
@@ -4,17 +4,19 @@ type Props = {
 
 const Tags: React.FC<Props> = ({ tags }) => {
   return (
-    <div className='xl:mt-2'>
+    <>
       {tags &&
-        tags.map((tag) => (
-          <span
-            key={tag}
-            className='dark:hover:bg-deePink mr-3 text-sm font-semibold italic hover:bg-deepBlue dark:hover:bg-deepPink xl:mr-0 xl:rounded-xl xl:px-3 xl:py-2 xl:hover:cursor-pointer'
-          >
-            #{tag}
-          </span>
-        ))}
-    </div>
+        tags.map((tag) => {
+          return (
+            <span
+              key={tag}
+              className='cursor-pointer rounded-sm px-3 py-2 font-semibold hover:bg-light-bg dark:hover:bg-dark-bg'
+            >
+              #{tag}
+            </span>
+          );
+        })}
+    </>
   );
 };
 
