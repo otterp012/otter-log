ec4fda97adce76f2998cb503e529d65306b5b90e
otterp012 <otterp012@gmail.com>
2023-01-30 17:21:36 +0900

    fix: constants에 필요없는 내용 제거

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,18 +1,3 @@
-export const NAV_PATHS = [
-  { path: "/post", title: "POST" },
-  { path: "/projects", title: "PROJECTS" },
-  { path: "/search", title: "SEARCH" },
-  { path: "/note?filter=React", title: "NOTES" },
-];
-
-export const NOTE_FILTER_OPTIONS = [
-  { option: "REACT", query: "React" },
-  { option: "JAVASCRIPT", query: "Javascript" },
-  { option: "NEXT", query: "NextJS" },
-  { option: "CS", query: "CS" },
-];
-
-export const BLUR_IMAGE_INLINE_BASE64 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMMDPT/DwAD3gHy5v4ozQAAAABJRU5ErkJggg==";
+export const NAV_PATHS = [{ path: "/post", title: "POST" }];
 
 export const GITHUB_URL = "https://github.com/otterp012";
