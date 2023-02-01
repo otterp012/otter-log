47bcd8a8cb161fef0d6462d1d60b7f613031a4ea
otterp012 <otterp012@gmail.com>
2023-02-01 19:20:30 +0900

    fix: 위로가기 버튼 스타일 수정 #40

--- components/ToTopButton/ToTopButton.tsx
+++ components/ToTopButton/ToTopButton.tsx
@@ -1,28 +1,9 @@
 import { memo } from "react";
 
-// const ToTopButton = () => {
-//   return (
-//     <div className='mt-20 text-center'>
-//       <a
-//         className='hover:text-yellow-300 cursor-pointer px-3 py-5 italic '
-//         onClick={(e) => {
-//           e.preventDefault();
-//           window.scrollTo({
-//             top: 0,
-//             behavior: "smooth",
-//           });
-//         }}
-//       >
-//         맨 위로가기 ⬆️⬆️
-//       </a>
-//     </div>
-//   );
-// };
-
 const ToTopButton = () => {
   return (
     <button
-      className='ml-5 hidden text-sm hover:text-blue lg:block'
+      className='text-md hidden w-full rounded-md pl-5 text-left hover:bg-deepGray lg:block'
       onClick={(e) => {
         e.preventDefault();
         window.scrollTo({
