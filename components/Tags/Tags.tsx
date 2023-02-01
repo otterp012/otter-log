39a652a1cce86ec8c963b5bff6813976edf24ca0
otterp012 <otterp012@gmail.com>
2023-02-01 17:08:15 +0900

    fix: custom link 컴포넌트 Props 수정 (query를 받을 수 있도록)

--- components/Tags/Tags.tsx
+++ components/Tags/Tags.tsx
@@ -1,19 +1,30 @@
+import { CustomLink } from "components/CustomLink";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 type Props = {
   tags: string[];
 };
 
+const ABLE = ["javascript", "react", "next"];
 const Tags: React.FC<Props> = ({ tags }) => {
   return (
     <>
       {tags &&
         tags.map((tag) => {
           return (
-            <span
+            <Link
+              href={{
+                pathname: "/post",
+                query: { filterBy: ABLE.includes(tag) ? tag : "others" },
+              }}
               key={tag}
-              className='cursor-pointer rounded-sm px-3 py-2 font-semibold hover:bg-light-bg dark:hover:bg-dark-bg'
+              passHref
             >
-              #{tag}
-            </span>
+              <span className='cursor-pointer rounded-sm px-3 py-2 font-semibold hover:bg-light-bg dark:hover:bg-dark-bg'>
+                #{tag}
+              </span>
+            </Link>
           );
         })}
     </>
