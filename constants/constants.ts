77a7a3e582dfa3c0cf2b230369c3a28a008f31ea
otterp012 <otterp012@gmail.com>
2023-02-01 17:09:35 +0900

    fi x: constants에 filter 관련 내용 추가

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,3 +1,6 @@
 export const NAV_PATHS = [{ path: "/post", title: "POST" }];
 
 export const GITHUB_URL = "https://github.com/otterp012";
+
+export const FILTER_OPTIONS = ["javascript", "react", "next", "others"];
+export const ABLE_FILTER_OPTIONS = ["react", "javascript", "next"];
