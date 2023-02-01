53d916cb921a0ba8302d262d25dfc7b190ce9a08
otterp012 <otterp012@gmail.com>
2023-02-01 21:37:17 +0900

    fix: 파서 함수로 page 수정

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,32 +2,17 @@ import * as React from "react";
 
 import { TableOfContents, MarkDown, PostHeader } from "components";
 import { getAllPublished, getPost } from "lib/notion";
+import { parseHeading } from "lib/utils";
 import type { HeadingType, MetaData, Params, PostType } from "types/types";
 
 const Post = ({ post }: { post: PostType }) => {
   const { markdown, headings, metadata } = post;
 
-  const regXHeader = /(^#{1,6})\s/g;
-  const parsedHeading = headings
-    .filter((item: { type: string; parent: string }) =>
-      item.type.includes("heading"),
-    )
-    .map((item: { type: string; parent: string }) => {
-      return {
-        heading: item.type.replace("_", ""),
-        slug: item.parent
-          .replace(regXHeader, "")
-          .replace(/ /g, "-")
-          .toLowerCase(),
-        text: item.parent.replace(regXHeader, ""),
-      };
-    }) as HeadingType[];
-
   return (
     <section className='mt-10'>
       <PostHeader {...metadata} />
       <div className='mt-5 lg:flex lg:flex-row-reverse'>
-        <TableOfContents headings={parsedHeading} />
+        <TableOfContents headings={headings} />
         <MarkDown markdownString={markdown} />
       </div>
     </section>
@@ -39,10 +24,25 @@ export default Post;
 export const getStaticProps = async (context: { params: Params }) => {
   const { slug } = context.params;
   const post = await getPost(slug as string);
+  const parsedHeading = post.headings
+    .filter((item: { type: string; parent: string }) =>
+      item.type.includes("heading"),
+    )
+    .map((item: { type: string; parent: string }) => {
+      const parsedText = parseHeading(item.parent);
+      return {
+        heading: item.type.replace(/_/, ""),
+        slug: parsedText.replace(/ /g, "-").toLowerCase(),
+        text: parsedText,
+      };
+    }) as HeadingType[];
 
   return {
     props: {
-      post,
+      post: {
+        ...post,
+        headings: parsedHeading,
+      },
     },
     revalidate: 60,
   };
