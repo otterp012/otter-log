493ab01165db98b1aeedd36b2f8a93d9de52e0b3
otterp012 <otterp012@gmail.com>
2023-02-03 15:37:21 +0900

    chore: constanst 에 Page url 추가

--- constants/constants.ts
+++ constants/constants.ts
@@ -4,3 +4,5 @@ export const GITHUB_URL = "https://github.com/otterp012";
 
 export const FILTER_OPTIONS = ["javascript", "react", "next", "others"];
 export const ABLE_FILTER_OPTIONS = ["react", "javascript", "next"];
+
+export const PAGE_URL = "https://otter-log.world";
