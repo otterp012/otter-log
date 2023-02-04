e4b71aeed0fafe8c22f064d864027a86a2ae3b75
otterp012 <otterp012@gmail.com>
2023-02-04 15:32:51 +0900

    fix: tag 컴포넌트 span -> link

--- components/Tags/Tags.tsx
+++ components/Tags/Tags.tsx
@@ -1,30 +1,28 @@
 import { CustomLink } from "components/CustomLink";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import { ABLE_FILTER_OPTIONS } from "constants/constants";
 
 type Props = {
   tags: string[];
 };
 
-const ABLE = ["javascript", "react", "next"];
 const Tags: React.FC<Props> = ({ tags }) => {
   return (
     <>
       {tags &&
         tags.map((tag) => {
           return (
-            <Link
+            <CustomLink
+              key={tag}
+              className='cursor-pointer rounded-sm px-3 py-2 font-semibold hover:bg-deepGray'
               href={{
                 pathname: "/post",
-                query: { filterBy: ABLE.includes(tag) ? tag : "others" },
+                query: {
+                  filterBy: ABLE_FILTER_OPTIONS.includes(tag) ? tag : "others",
+                },
               }}
-              key={tag}
-              passHref
             >
-              <span className='cursor-pointer rounded-sm px-3 py-2 font-semibold hover:bg-light-bg dark:hover:bg-dark-bg'>
-                #{tag}
-              </span>
-            </Link>
+              #{tag}
+            </CustomLink>
           );
         })}
     </>
