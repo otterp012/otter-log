67117226d508be53eee701ebc45440715c6e0371
otterp012 <otterp012@gmail.com>
2023-02-06 16:38:43 +0900

    fix: metadata, heading 타입 명확하게 수정

--- components/Article/ArticleHeader.tsx
+++ components/Article/ArticleHeader.tsx
@@ -4,15 +4,24 @@ import { Tags } from "components/Tags";
 
 import type { MetaData } from "types/types";
 
-export const ArticleHeader = (props: MetaData) => {
-  const { title, cover, tags, last_edit, date, last_mod } = props;
+type ArticleHeaderProps = Omit<MetaData, "id" | "description" | "slug">;
+
+export const ArticleHeader = (props: ArticleHeaderProps) => {
+  const {
+    title,
+    thumbnailImg,
+    tags,
+    formattedDate,
+    lastEditDate,
+    lastEditFormattedDate,
+  } = props;
   return (
     <header className='space-y-3 border-b border-b-gray pb-8 dark:border-b-deepGray'>
       <h2 className='break-keep mb-5 text-center text-4xl font-bold md:text-5xl'>
         {title}
       </h2>
       <Image
-        src={cover}
+        src={thumbnailImg}
         alt={title}
         className='mx-auto h-auto w-[90%] rounded-xl object-cover md:w-[640px]'
         width={1000}
@@ -23,14 +32,17 @@ export const ArticleHeader = (props: MetaData) => {
         <div>
           <Tags tags={tags} />
         </div>
-        <time dateTime={last_mod.toString()} className='mr-2 inline text-xs'>
-          {date}
+        <time
+          dateTime={lastEditDate.toString()}
+          className='mr-2 inline text-xs'
+        >
+          {formattedDate}
         </time>
         <span className='text-xs'>by otter</span>
-        <em className='block text-xs'>
-          {last_edit}에 최종수정되었습니다. <br />
+        <span className='block text-xs'>
+          {lastEditFormattedDate}에 최종수정되었습니다. <br />
           잘못된 내용이 있으면 댓글을 달아주세요.
-        </em>
+        </span>
       </div>
     </header>
   );
--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -4,44 +4,29 @@ import { MetaData } from "types/types";
 import { ImageWithFallback } from "components/ImageWithFallback";
 import { getRevisedImageUrl } from "lib/utils";
 
-export type Props = {
-  title: string;
-  description: string;
-  publishedAt?: string;
-  thumbnailImg: string;
-  slug: string;
-  tags: string[];
-  lastMod: Date;
-};
-
 export const Cards = ({ posts }: { posts: MetaData[] }) => {
   return (
     <ul className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
-      {posts.map((post: MetaData, index) => (
-        <Card
-          title={post.title}
-          description={post.description}
-          publishedAt={post.date}
-          thumbnailImg={post.cover}
-          key={post.title}
-          slug={post.slug}
-          tags={post.tags}
-          lastMod={post.last_mod}
-        />
+      {posts.map((post: MetaData) => (
+        <Card {...post} key={post.id} />
       ))}
     </ul>
   );
 };
 
-export const Card: React.FC<Props> = ({
-  title,
-  publishedAt,
-  description,
-  thumbnailImg,
-  slug,
-  tags,
-  lastMod,
-}) => {
+type CardProps = Omit<MetaData, "id" | "lastEditFormattedDate">;
+
+export const Card = (props: CardProps) => {
+  const {
+    title,
+    thumbnailImg,
+    tags,
+    formattedDate,
+    lastEditDate,
+    description,
+    slug,
+  } = props;
+
   return (
     <li className='mx-auto h-[400px] w-[95%] list-none rounded-lg bg-light-card shadow-lg dark:bg-dark-card sm:w-auto'>
       <ImageWithFallback
@@ -54,8 +39,11 @@ export const Card: React.FC<Props> = ({
         priority={true}
       />
       <div className='mt-4 flex flex-col items-center'>
-        <time className='font-semi-bold text-xs' dateTime={lastMod.toString()}>
-          {publishedAt}
+        <time
+          className='font-semi-bold text-xs'
+          dateTime={lastEditDate.toString()}
+        >
+          {formattedDate}
         </time>
         <h2 className='mb-2 text-center text-xl font-bold'>{title}</h2>
         <p className='break-keep h-10 w-[80%] text-center text-sm line-clamp-2'>
