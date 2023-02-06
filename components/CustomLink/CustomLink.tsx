f830b9228cfd4919e24e0869075ebaf869aefb16
otterp012 <otterp012@gmail.com>
2023-02-06 13:35:34 +0900

    fix: custom Link 컴포넌트 접근성 수정

--- components/CustomLink/CustomLink.tsx
+++ components/CustomLink/CustomLink.tsx
@@ -3,14 +3,17 @@ import Link, { LinkProps } from "next/link";
 interface CustomLinkProps extends LinkProps {
   children: React.ReactNode;
   className?: string;
+  title?: string;
 }
 
 const CustomLink = (props: CustomLinkProps) => {
-  const { children, className, ...restProps } = props;
+  const { children, className, title, ...restProps } = props;
 
   return (
     <Link {...restProps} passHref>
-      <a className={className}>{children}</a>
+      <a className={className} title={title} aria-label={title}>
+        {children}
+      </a>
     </Link>
   );
 };
