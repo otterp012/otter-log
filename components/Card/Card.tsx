f6a53bc13cded67f3f8a7dcbe6dd0d1e516c44c2
otterp012 <otterp012@gmail.com>
2023-02-06 14:12:58 +0900

    fix: Card 컴포넌트 이미지 크기를 줄여서 받아오도록 수정

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,14 +1,14 @@
-import Image from "next/future/image";
-
-// component
 import { CustomLink } from "components/CustomLink";
 import { Tags } from "components/Tags";
 import { MetaData } from "types/types";
+import { ImageWithFallback } from "components";
+import { getRevisedImageUrl } from "lib/utils";
+
 export type Props = {
   title: string;
   description: string;
   publishedAt?: string;
-  thumbnailImg: string | undefined;
+  thumbnailImg: string;
   slug: string;
   tags: string[];
   lastMod: Date;
@@ -16,8 +16,8 @@ export type Props = {
 
 const Cards = ({ posts }: { posts: MetaData[] }) => {
   return (
-    <ul className='flex flex-wrap gap-2'>
-      {posts.map((post: MetaData) => (
+    <ul className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
+      {posts.map((post: MetaData, index) => (
         <Card
           title={post.title}
           description={post.description}
@@ -43,29 +43,31 @@ const Card: React.FC<Props> = ({
   lastMod,
 }) => {
   return (
-    <li className='mx-auto mb-5 h-96 w-[95%] list-none rounded-lg bg-light-card shadow-lg dark:bg-dark-card sm:w-[48%] lg:w-[32%]'>
-      <Image
-        src={thumbnailImg ?? ""}
+    <li className='mx-auto h-[400px] w-[95%] list-none rounded-lg bg-light-card shadow-lg dark:bg-dark-card sm:w-auto'>
+      <ImageWithFallback
+        src={getRevisedImageUrl({ src: thumbnailImg })}
+        fallbackSrc={getRevisedImageUrl({ src: thumbnailImg, format: "webp" })}
         alt={title}
-        width={1000}
-        height={1000}
+        width={600}
+        height={400}
         className='h-[50%] rounded-t-lg object-cover'
-        blurDataURL={
-          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMMDPT/DwAD3gHy5v4ozQAAAABJRU5ErkJggg=="
-        }
+        priority={true}
       />
       <div className='mt-4 flex flex-col items-center'>
         <time className='font-semi-bold text-xs' dateTime={lastMod.toString()}>
           {publishedAt}
         </time>
         <h2 className='mb-2 text-center text-xl font-bold'>{title}</h2>
-        <p className='text-sm'>{description}</p>
+        <p className='break-keep h-10 w-[80%] text-center text-sm line-clamp-2'>
+          {description}
+        </p>
         <div className='mt-2'>
           <Tags tags={tags} />
         </div>
         <CustomLink
           href={`post/${slug}`}
-          className='mt-5 rounded-md bg-light-bg px-5 py-2 font-semibold shadow-2xl hover:bg-deepGray dark:bg-dark-bg hover:dark:bg-deepGray'
+          className='mt-2 rounded-md bg-light-bg px-5 py-2 font-semibold shadow-2xl hover:bg-deepGray dark:bg-dark-bg hover:dark:bg-deepGray'
+          title={title}
         >
           READ MORE
         </CustomLink>
