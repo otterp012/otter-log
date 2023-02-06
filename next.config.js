2bad564c1e574f25189d46637c7b32bbabfec78a
otterp012 <otterp012@gmail.com>
2023-02-06 14:10:23 +0900

    chore: 디펜던시 정리

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -5,5 +9,4 @@ const nextConfig = {
     disableStaticImages: true,
   },
 };
-
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
