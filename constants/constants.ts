e50563f82a7bc8eb3e277f480d37ad4a76f8fba1
otterp012 <otterp012@gmail.com>
2023-02-13 12:55:36 +0900

    fix: contansts에 Book 관련 데이터 추가

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,4 +1,7 @@
-export const NAV_PATHS = [{ path: "/post", title: "POST" }];
+export const NAV_PATHS = [
+  { path: "/post", title: "POST" },
+  { path: "/book", title: "BOOK" },
+];
 
 export const GITHUB_URL = "https://github.com/otterp012";
 
@@ -6,3 +9,22 @@ export const FILTER_OPTIONS = ["javascript", "react", "next", "others"];
 export const ABLE_FILTER_OPTIONS = ["react", "javascript", "next"];
 
 export const PAGE_URL = "https://otter-log.world";
+
+// 추후에 Api에서 직접 받아오는 방향으로 수정할 예정
+export const BOOKS_INFO = [
+  {
+    title: "단위 테스트",
+    id: "unittest",
+    reference: "단위 테스트, 블라디미르 코리코프 저/임준혁 역",
+  },
+  {
+    title: "모던 자바스크립트 Deep Dive",
+    id: "deepdive",
+    reference: "모던 자바스크립트 Deep Dive, 이웅모 저",
+  },
+  {
+    title: "TIL",
+    id: "til",
+    reference: "otter의 TIL기록장",
+  },
+];
