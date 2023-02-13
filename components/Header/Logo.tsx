e9c001fe55a5627dda4b36cff218285995eea92c
otterp012 <otterp012@gmail.com>
2023-02-13 12:41:08 +0900

    fix: BOOK Nav 추가로 인해, 모바일 뷰에서의 스타일 수정 (폰트 크기)

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -1,10 +1,14 @@
+import { memo } from "react";
+
 // component
 import { CustomLink } from "components/CustomLink";
-import { memo } from "react";
 
 export const Logo = memo(() => {
   return (
-    <h1 aria-label='otter-log' className='text-3xl font-extrabold italic'>
+    <h1
+      aria-label='otter-log'
+      className='text-lg font-extrabold italic md:text-3xl'
+    >
       <CustomLink href='/'>OTTER-LOG</CustomLink>
     </h1>
   );
