9b755bfd311940c615fbd00cdd50aefa8b8db016
otterp012 <otterp012@gmail.com>
2023-02-26 13:18:29 +0900

    chore: intersection observer mock 제거

--- __mock__/intersectionObserver.ts
+++
@@ -1,6 +0,0 @@
-export const intersectionObserverMock = () => ({
-  observe: () => null,
-  disconnect: () => null,
-});
-
-// https://stackoverflow.com/questions/57008341/jest-testing-react-component-with-react-intersection-observer
