dab2e7c5bbfea8cddf3d38ddb07731e287d8a7df
otterp012 <otterp012@gmail.com>
2023-02-27 00:30:19 +0900

    test: ToTopButton 컴포넌트 테스트코드 userEvent로 수정

--- components/ToTopButton/ToTopButton.test.tsx
+++ components/ToTopButton/ToTopButton.test.tsx
@@ -1,14 +1,16 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { ToTopButton } from "./ToTopButton";
+import userEvent from "@testing-library/user-event";
 
 describe("ToTopButton", () => {
-  it("ToTopButton버튼을 누르면, 페이지가 맨 위로 이동된다.", () => {
+  it("ToTopButton버튼을 누르면, 페이지가 맨 위로 이동된다.", async () => {
+    const user = userEvent.setup();
     window.scroll = jest.fn();
 
     render(<ToTopButton />);
 
     const toTopBtn = screen.getByText(/맨 위로 가기/);
-    fireEvent.click(toTopBtn);
+    await user.click(toTopBtn);
     expect(window.scroll).toHaveBeenCalledWith({
       top: 0,
       behavior: "smooth",
