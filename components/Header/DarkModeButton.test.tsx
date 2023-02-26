f203a2d278225055cd0a6d4767ef5cd89064a02c
otterp012 <otterp012@gmail.com>
2023-02-27 00:04:29 +0900

    test: dark mode button 테스트코드 수정

--- components/Header/DarkModeButton.test.tsx
+++ components/Header/DarkModeButton.test.tsx
@@ -1,33 +1,48 @@
-import { render, RenderResult } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-
+import { axe } from "jest-axe";
 import { DarkModeButton } from "./DarkModeButton";
+import { DarkModeProvider } from "store";
 
-describe("dark mode", () => {
-  let rendered: RenderResult;
-  let button: HTMLElement;
-
-  beforeEach(() => {
-    // arrange
-    rendered = render(<DarkModeButton />);
-    button = rendered.getByRole("button");
+const setup = () => {
+  const user = userEvent.setup();
+  const view = render(<DarkModeButton />, {
+    wrapper: DarkModeProvider,
   });
 
-  it("darkMode 버튼이 문제없이 렌더링 되어야 한다.", () => {
-    expect(button).toBeInTheDocument();
+  const button = () => screen.getByRole("button");
+
+  return {
+    user,
+    view,
+    button,
+  };
+};
+
+describe("DartModeButton", () => {
+  it("접근성 위반사항이 없어야 한다.", async () => {
+    const { view } = setup();
+
+    const results = await axe(view.container);
+
+    expect(results).toHaveNoViolations();
   });
 
-  it("darkMode 버튼을 클릭하면 다크모드로 바뀌고, 다시한번 누르면 라이트 모드로 바뀐다.", async () => {
-    // act
-    await userEvent.click(button);
+  it("다크모드 버튼은 초기에, 라이트 모드로 렌더링 된다.", () => {
+    const { button } = setup();
 
-    // assert
-    expect(button).toHaveAccessibleName("LIGHT");
+    expect(button()).toHaveAccessibleName("현재 라이트 모드");
+  });
 
-    // act
-    await userEvent.click(button);
+  it("다크모드 버튼을 클릭하면, 모드가 변경된다.", async () => {
+    const { button, user } = setup();
 
-    // assert
-    expect(button).toHaveAccessibleName("DARK");
+    const renderedButton = button();
+    await user.click(renderedButton);
+
+    expect(renderedButton).toHaveAccessibleName("현재 다크 모드");
+
+    await user.click(renderedButton);
+    expect(renderedButton).toHaveAccessibleName("현재 라이트 모드");
   });
 });
