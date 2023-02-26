3e98cf6c7fd55d029c7df996b9bb4a78d37c5f6f
otterp012 <otterp012@gmail.com>
2023-02-27 01:13:19 +0900

    test: header 컴포넌트 테스트코드 수정

--- components/Header/Header.test.tsx
+++ components/Header/Header.test.tsx
@@ -1,22 +1,43 @@
 // 모든페이지에 렌더링되는 layout 컴포넌트 테스트
 import { render, screen } from "@testing-library/react";
+import { axe } from "jest-axe";
 import { RouterContext } from "next/dist/shared/lib/router-context";
-import { createMockRouter } from "__mock__/next";
-
+import { DarkModeProvider } from "store";
+import { createMockRouter } from "__mock__";
 import { Header } from "./Header";
 
-describe("Header", () => {
-  it("Header 컴포넌트가 렌더링 된다.", async () => {
-    const router = createMockRouter({ pathname: "/" });
-    render(
-      <RouterContext.Provider value={router}>
-        <Header />
-      </RouterContext.Provider>,
-    );
+const setup = () => {
+  const router = createMockRouter({ pathname: "/" });
+  const view = render(
+    <RouterContext.Provider value={router}>
+      <Header />
+    </RouterContext.Provider>,
+    { wrapper: DarkModeProvider },
+  );
 
-    const Heading = screen.getByRole("heading", { name: /otter-log/i });
-    const Nav = screen.getByRole("navigation");
-    expect(Heading).toBeInTheDocument();
-    expect(Nav).toBeInTheDocument();
+  const header = () => screen.getByRole("banner");
+  const heading = () => screen.getByRole("heading");
+  const nav = () => screen.getByRole("navigation");
+
+  return {
+    view,
+    header,
+    heading,
+    nav,
+  };
+};
+describe("Header", () => {
+  it("문제없이 렌더링 되어야 한다.", () => {
+    const { header, heading, nav } = setup();
+    expect(header()).toBeInTheDocument();
+    expect(heading()).toBeInTheDocument();
+    expect(nav()).toBeInTheDocument();
+  });
+
+  it("접근성 위반사항이 없어야 한다.", async () => {
+    const { view } = setup();
+    const { container } = view;
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });
