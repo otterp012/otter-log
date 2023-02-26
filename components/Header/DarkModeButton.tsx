71c9be164e5e25b6db5cc5ef7795d4ddf8c953d8
otterp012 <otterp012@gmail.com>
2023-02-27 00:03:42 +0900

    fix: 다크모드버튼에 label 추가

--- components/Header/DarkModeButton.tsx
+++ components/Header/DarkModeButton.tsx
@@ -4,8 +4,10 @@ import { useDarkModeContext } from "store";
 export const DarkModeButton = memo(() => {
   const { themeIsDark, themeModeHandler } = useDarkModeContext();
 
+  const label = themeIsDark ? "현재 다크 모드" : "현재 라이트 모드";
   return (
     <button
+      aria-label={label}
       className='rounded-xl bg-black px-2 py-1 text-xl font-semibold text-white dark:bg-white dark:text-black'
       onClick={themeModeHandler}
     >
