629693237b25369232a022e176c9748df90599a2
otterp012 <otterp012@gmail.com>
2023-02-27 12:25:11 +0900

    style: ToTopButton 폰트 크기 축소

--- components/ToTopButton/ToTopButton.tsx
+++ components/ToTopButton/ToTopButton.tsx
@@ -4,7 +4,7 @@ import { memo } from "react";
 export const ToTopButton = memo(() => {
   return (
     <a
-      className='text-md hidden w-full cursor-pointer rounded-md pl-5 text-left hover:bg-deepGray lg:block'
+      className='hidden w-full cursor-pointer rounded-md pl-5 text-left text-sm hover:bg-deepGray lg:block'
       onClick={scrollTopHandler}
     >
       맨 위로 가기
